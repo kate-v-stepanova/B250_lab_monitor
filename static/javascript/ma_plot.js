1c8b50a589d26f5baa5aec36f6e7fc3901c7daf2
b250-admin <b250-admin@b2502-mb04doc.inet.dkfz-heidelberg.de>
2019-07-10 12:02:47 +0200

    MA plot: improve filters

--- static/javascript/ma_plot.js
+++ static/javascript/ma_plot.js
@@ -1,17 +1,52 @@
 $(document).ready(function() {
+    // close error message
     $('span.close').on('click', function() {
         $(this).parent().parent().parent().addClass('d-none');
     });
 
-    if ($("div.ma_plot").length != 0) {
-        var contrast = $('div.ma_plot').attr('data-contrast');
+    // apply filters
+    $("#apply_filters").on('change', function() {
+        if(this.checked) {
+          $('#data_filters').removeClass('d-none');
+        } else {
+            $('#data_filters').addClass('d-none');
+        }
+    });
+
+    if ($("#ma_plot").length != 0) {
+        var contrast = $('#ma_plot').attr('data-contrast');
         contrast = contrast.replace(/__/g, ' ');
-        var plot_series = $('div.ma_plot').attr('data-series');
+        var plot_series = $('#ma_plot').attr('data-series');
         plot_series = plot_series.replace(/'/g, '"'); //");
         if (plot_series.length != 0) {
             plot_series = JSON.parse(plot_series);
         }
-        Highcharts.chart('ma_plot', {
+        var genes = $('#ma_plot').attr('data-genes');
+        var xmax = $('#ma_plot').attr('data-xmax');
+        var ymax = $('#ma_plot').attr('data-ymax');
+        var ymin = -1 * ymax;
+        var filters = $('#ma_plot').attr('data-filters');
+        var x_axis = {
+            title: {
+                enabled: true,
+                text: 'Normalized counts'
+            },
+            startOnTick: true,
+            endOnTick: true,
+            showLastLabel: true,
+            min: 0,
+        }
+        var y_axis = {
+            title: {
+                text: 'Log2 fold change'
+            }
+        }
+        if (filters == "True") {
+            x_axis["max"] = parseInt(xmax);
+            y_axis["min"] = parseFloat(ymin);
+            y_axis["max"] = parseFloat(ymax);
+        }
+        var ch = Highcharts.chart('ma_plot', {
             chart: {
                 type: 'scatter',
                 zoomType: 'xy'
@@ -19,27 +54,17 @@ $(document).ready(function() {
             title: {
                 text: contrast
             },
-            xAxis: {
-                title: {
-                    enabled: true,
-                    text: 'Mean of normalized counts'
-                },
-                startOnTick: true,
-                endOnTick: true,
-                showLastLabel: true,
-                min: 0
+            subtitle: {
+                text: "Number of genes: " + genes,
             },
-            yAxis: {
-                title: {
-                    text: 'Log2 fold change'
-                }
-            },
-
+            xAxis: x_axis,
+            yAxis: y_axis,
             plotOptions: {
                 scatter: {
                     tooltip: {
                         headerFormat: '',
-                        pointFormat: '<b>Counts: </b>{point.x:.2f}<br><b>Log2 fc: </b>{point.y:.2f}<br><b>Transcript: </b>{point.transcript}'
+                        pointFormat: '<b>Counts: </b>{point.x:.0f}<br><b>Log2 fc: </b>{point.y:.2f}<br><b>p value: </b>' +
+                            '{point.pvalue:.2f}<br><b>Transcript: </b>{point.transcript}'
                     }
                 },
                 series: {
@@ -51,6 +76,9 @@ $(document).ready(function() {
                     },
                     color: "#204060",
                 }
+            },
+            legend: {
+                enabled: false
             },
             series: [plot_series]
         });
--- url_handlers/ma_plot.py
+++ url_handlers/ma_plot.py
@@ -14,20 +14,45 @@ def get_ma_plot(project_id):
 
     if request.method == "GET":
         no_data = len(contrasts) == 0 # if no contrasts, no_data will be True
+        #
+        # df = pd.read_msgpack(bi_df)
         return render_template("ma_plot.html", contrasts=contrasts, no_data=no_data)
 
     contrast = request.form.get('contrast')
-
+    apply_filters = request.form.get('apply_filters') == "true" # is not None
     pval = request.form.get('pval')
     fc = request.form.get('fc')
+    min_counts = request.form.get('min_counts')
+    max_counts = request.form.get('max_counts')
+    filters = {
+        'pval': pval,
+        'fc': fc,
+        'min_counts': min_counts,
+        'max_counts': max_counts,
+    }
     bi_df = rdb.get('ma_plot_{}_{}'.format(project_id, contrast))
     if not bi_df:
-        return render_template("ma_plot.html", error="No data for the contrast {}".format(contrast), pval=pval, fc=fc,
-                               selected_contrast=contrast, contrasts=contrasts)
+        return render_template("ma_plot.html", error="No data for the contrast {}".format(contrast),
+                               selected_contrast=contrast, contrasts=contrasts, apply_filters=apply_filters,
+                               filters=filters)
     df = pd.read_msgpack(bi_df)
     # columns: ['baseMean', 'log2FoldChange', 'lfcSE', 'stat', 'pvalue', 'padj', 'transcript']
     df = df.rename({'baseMean': 'x', 'log2FoldChange': 'y'}, axis='columns')
+    if apply_filters:
+        if pval:
+            pval = float(pval)
+            df = df.loc[df["pvalue"] >= pval]
+        if fc:
+            fc = float(fc)
+            df = df.loc[(df["y"] <= fc) & (df["y"] >= -1 * fc)]
+        if min_counts:
+            min_counts = int(min_counts)
+            df = df.loc[df["x"] >= min_counts]
+        if max_counts:
+            max_counts = int(max_counts)
+            df = df.loc[df["x"] <= max_counts]
+
     plot_series = {'name': contrast.replace('__', ' '),
                    'data': df.to_dict('records')}
-    return render_template("ma_plot.html", contrasts=contrasts, selected_contrast=contrast, pval=pval, fc=fc,
-                           plot_series=plot_series)
+    return render_template("ma_plot.html", contrasts=contrasts, selected_contrast=contrast, plot_series=plot_series,
+                           apply_filters=apply_filters, genes=len(df), filters=filters)
