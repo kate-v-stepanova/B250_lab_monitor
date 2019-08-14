76edd10a90a5931e6141403c3df960c09484be0b
b250-admin <b250-admin@b2502-mb04doc.inet.dkfz-heidelberg.de>
2019-08-14 14:09:21 +0200

    Heatmap: 2 groups of samples to compare

--- static/javascript/heatmap.js
+++ static/javascript/heatmap.js
@@ -4,40 +4,53 @@ $(document).ready(function() {
         $(this).parent().parent().parent().addClass('d-none');
     });
 
+    // initial html of multiselect without any selected options
+    const group2_html = $('li#group2').html().replace(/selected/g, '');
 
-    const choices = new Choices('#selected_samples', {
+    const choices = new Choices('#first_group', {
         removeItems: true,
         removeItemButton: true,
         noChoicesText: "All samples selected"
     });
 
-    $('#filter_by').change(function() {
+    var group2 = new Choices('#second_group', {
+        removeItems: true,
+        removeItemButton: true,
+        noChoicesText: "All samples selected"
+    });
+
+
+    $('#filter1').change(function() {
         if ($(this).val() === 'list_of_genes') {
             $('#list_of_genes').removeClass('d-none');
             $('#number_of_genes').addClass('d-none');
+            remove_group();
+            $('#add_group').addClass('d-none');
         } else {
             if (!$('#list_of_genes').hasClass('d-none')) {
                 $('#list_of_genes').addClass('d-none');
                 $('#number_of_genes').removeClass('d-none');
+                $('#add_group').removeClass('d-none');
             }
-
         }
     });
-    var a = $('#inchlib').attr('data-series');
-    a = a.replace(/'/g, '"'); //");
-    a = JSON.parse(a);
-    $('#inchlib').removeAttr('data-series');
-    window.inchlib = new InCHlib({ //instantiate InCHlib
-        target: "inchlib", //ID of a target HTML element
-        metadata: true, //turn on the metadata
-        draw_row_ids: true,
-        max_height: 1200, //set maximum height of visualization in pixels
-        width: 1000, //set width of visualization in pixels
-        heatmap_colors: "BuWhRd", //set color scale for clustered data
-        column_dendrogram: true,
-    });
-    inchlib.read_data(a);
-    inchlib.draw(); //draw cluster heatmap
+    if ($('#inchlib').length != 0) {
+        var a = $('#inchlib').attr('data-series');
+        a = a.replace(/'/g, '"'); //");
+        a = JSON.parse(a);
+        $('#inchlib').removeAttr('data-series');
+        window.inchlib = new InCHlib({ //instantiate InCHlib
+            target: "inchlib", //ID of a target HTML element
+            metadata: true, //turn on the metadata
+            draw_row_ids: true,
+            max_height: 1200, //set maximum height of visualization in pixels
+            width: 1000, //set width of visualization in pixels
+            heatmap_colors: "BuWhRd", //set color scale for clustered data
+            column_dendrogram: true,
+        });
+        inchlib.read_data(a);
+        inchlib.draw(); //draw cluster heatmap
+    }
 
     // export CSV data
     var csv_data = $('#inchlib').attr('data-csv')
@@ -47,4 +60,32 @@ $(document).ready(function() {
             saveAs(blob, "heatmap.csv");
         });
     }
+
+    // add group of samples
+    $('#add_group').on('click', function() {
+        $('#group2').removeClass('d-none');
+        $('#add_group').addClass('d-none');
+        $('#info').removeClass('d-none');
+    });
+
+    // remove group of samples
+    $('#remove_group').on('click', remove_group);
+    function remove_group() {
+        // clear Choices
+        group2.destroy();
+        $('li#group2').empty();
+        $('li#group2').append(group2_html);
+        group2 = new Choices('#second_group', {
+            removeItems: true,
+            removeItemButton: true,
+            noChoicesText: "All samples selected"
+        });
+        $('#add_group').removeClass('d-none').removeClass('d-none');
+        $('#group2').addClass('d-none');
+        $('#remove_group').on('click', remove_group);
+    }
+
+    // tooltips
+    $('#number_of_genes1').tooltip({'delay':0});
+    $('#number_of_genes2').tooltip({'delay':0});
 });
--- url_handlers/heatmap.py
+++ url_handlers/heatmap.py
@@ -23,46 +23,117 @@ def get_heatmap(project_id):
     samples.remove('gene_name')
 
     if request.method == "GET":
-        return render_template("heatmap.html", samples=samples, selected_samples=samples)
+        return render_template("heatmap.html", samples=samples, first_group=[], second_group=[])
 
-    selected_samples = request.form.getlist('selected_samples')
-    if not selected_samples:
+    first_group = request.form.getlist('first_group')
+    if not first_group:
         return render_template("heatmap.html", samples=samples, error="No samples selected")
 
-    filter_by = request.form.get('filter_by')
-    number_of_genes = int(request.form.get('number_of_genes'))
-    list_of_genes = request.form.get('list_of_genes', '').split()
+    second_group = request.form.getlist('second_group')
 
-    # filter by genes
-    if filter_by == "top_coding" or filter_by == "list_of_genes":
+    filter1 = request.form.get('filter1')
+    filter2 = request.form.get('filter2')
+
+    number_of_genes1 = int(request.form.get('number_of_genes1'))
+    number_of_genes2 = int(request.form.get('number_of_genes2'))
+
+    list_of_genes = request.form.get('list_of_genes', '').split()
+    include_non_coding = request.form.get('include_non_coding', "") == "True"
+
+    if include_non_coding:
+        data = rdb.get('cpm_non_coding_{}'.format(project_id))
+        df = pd.read_msgpack(data)
+    else:
         data = rdb.get('cpm_coding_{}'.format(project_id))
         df = pd.read_msgpack(data)
 
-    elif filter_by == "top_non_coding":
-        data = rdb.get('cpm_non_coding_{}'.format(project_id))
-        df = pd.read_msgpack(data)
-
-    # select samples and sort by variance
-    df = df[['gene_name'] + selected_samples]
-    for sample in selected_samples:
-        df[sample] = df[sample].round(2)
-
-    df = df.reindex(df.var(axis=1).sort_values(ascending=False).index)
-
-    # select genes
-    if filter_by == "top_coding" or filter_by == "top_non_coding":
-        df = df[:number_of_genes]
-
-    elif filter_by == "list_of_genes":
+    if filter1 == 'list_of_genes':
         df1 = None
-        # should be ok for ~100-200 genes
         for gene in list_of_genes:
             row = df.loc[df['gene_name'] == gene]
             df1 = row if df1 is None else df1.append(row, ignore_index=True)
-        df = df1
+
+            # input for clustering: header + df
+            data = [['gene_name'] + first_group] + df1.values.tolist()
+
+            cluster = Cluster()
+            cluster.read_data(rows=data, header=True)
+            cluster.cluster_data()
+            dendrogram = Dendrogram(cluster)
+            plot_data = dendrogram.create_cluster_heatmap()
+            plot_data = json.dumps(plot_data)
+
+            csv_data = df1.to_csv(sep=",", header=True, index=False)
+            return render_template("heatmap.html", plot_data=plot_data, first_group=first_group,
+                                   list_of_genes=list_of_genes, number_of_genes1=number_of_genes1, filter1=filter1,
+                                   number_of_genes2=number_of_genes2, include_non_coding=include_non_coding,
+                                   csv_data=csv_data, filter2=filter2)
+
+    # select samples
+    df1 = df[['gene_name'] + first_group]
+
+    # round to 2 decimals
+    for sample in first_group:
+        df1[sample] = df1[sample].round(2)
+
+    # sort by variance
+    if filter1 == 'least':
+        df1 = df1.reindex(df1.var(axis=1).sort_values(ascending=True).index)  # 1 2 3
+    else:
+        df1 = df1.reindex(df1.var(axis=1).sort_values(ascending=False).index)  # 3 2 1
+
+    # select top genes (or all if number_of_genes is 0)
+    if number_of_genes1 != 0:
+        df1 = df1[:number_of_genes1]
+
+    if not second_group:
+        # input for clustering: header + df
+        data = [['gene_name'] + first_group] + df1.values.tolist()
+
+        cluster = Cluster()
+        cluster.read_data(rows=data, header=True)
+        cluster.cluster_data()
+        dendrogram = Dendrogram(cluster)
+        plot_data = dendrogram.create_cluster_heatmap()
+        plot_data = json.dumps(plot_data)
+        csv_data = df1.to_csv(sep=",", header=True, index=False)
+
+        return render_template("heatmap.html", plot_data=plot_data, first_group=first_group,
+                   list_of_genes=list_of_genes, number_of_genes1=number_of_genes1, include_non_coding=include_non_coding,
+                   filter1=filter1, number_of_genes2=number_of_genes2, csv_data=csv_data, filter2=filter2)
+
+    # if second group
+    df2 = df[['gene_name'] + first_group + second_group]
+
+    # pairwise comparisons
+    df22 = None
+    for sample1 in first_group:
+        for sample2 in second_group:
+            df2_var = df[[sample1, sample2]].var(axis=1)
+            df22 = df2_var if df22 is None else df22 + df2_var
+    # average variance
+    df22 = df22 / len(second_group)
+    # sort by variance
+    ascending = filter2 == 'least'
+    df22 = df2.reindex(df22.sort_values(ascending=ascending).index)  # 3 2 1
+
+    if number_of_genes2 != 0:
+        df22 = df22[:number_of_genes2]
+
+    common_genes = list(set(df22['gene_name'].tolist()) & set(df1['gene_name'].tolist()))
+
+    if len(common_genes) == 0:
+        error = "No common genes found between 2 groups. Try to increase the number of genes"
+        return render_template("heatmap.html", first_group=first_group, second_group=second_group,
+               list_of_genes=list_of_genes, number_of_genes1=number_of_genes1, include_non_coding=include_non_coding,
+               filter1=filter1, number_of_genes2=number_of_genes2, filter2=filter2, error=error)
+
+    final_df = df2.loc[df2['gene_name'].isin(common_genes)]
+    for sample in first_group + second_group:
+        final_df[sample] = final_df[sample].round(2)
 
     # input for clustering: header + df
-    data = [['gene_name'] + selected_samples] + df.values.tolist()
+    data = [['gene_name'] + first_group + second_group] + final_df.values.tolist()
 
     cluster = Cluster()
     cluster.read_data(rows=data, header=True)
@@ -70,8 +141,9 @@ def get_heatmap(project_id):
     dendrogram = Dendrogram(cluster)
     plot_data = dendrogram.create_cluster_heatmap()
     plot_data = json.dumps(plot_data)
+    csv_data = df1.to_csv(sep=",", header=True, index=False)
 
-    csv_data = df.to_csv(sep=",", header=False, index=False)
-    return render_template("heatmap.html", samples=samples, selected_samples=selected_samples,
-                           number_of_genes=number_of_genes, filter_by=filter_by, list_of_genes=list_of_genes,
-                           plot_data=plot_data, csv_data=csv_data)
+    return render_template("heatmap.html", samples=samples, first_group=first_group, second_group=second_group,
+                           number_of_genes1=number_of_genes1, number_of_genes2=number_of_genes2, filter1=filter1,
+                           filter2=filter2, list_of_genes=list_of_genes, include_non_coding=include_non_coding,
+                           plot_data=plot_data, csv_data=csv_data, common_genes=len(final_df))
